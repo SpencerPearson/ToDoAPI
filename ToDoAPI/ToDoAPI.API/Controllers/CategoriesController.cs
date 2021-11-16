693cf46942b6ae3244e387498d7f7f47496fcaa6
SpencerPearson <spencer.pearson.816@outlook.com>
2021-11-16 14:51:34 -0600

    built out Categories controller

---
+++ ToDoAPI/ToDoAPI.API/Controllers/CategoriesController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToDoAPI.API.Models;
+using ToDoAPI.DATA.EF;
+using System.Web.Http.Cors;
+
+namespace ToDoAPI.API.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class CategoriesController : ApiController
+    {
+        ToDoEntities db = new ToDoEntities();
+
+        //api/Categories
+        public IHttpActionResult GetCategories()
+        {
+            List<CategoryViewModel> cats = db.Categories.Select(c => new CategoryViewModel()
+            {
+                CategoryId = c.CategoryId,
+                Name = c.Name,
+                Description = c.Description
+            }).ToList<CategoryViewModel>();
+
+            if (cats.Count == 0)
+                return NotFound();
+
+            return Ok(cats);
+        }
+
+        //api/Categories/id
+        public IHttpActionResult GetCategory(int id)
+        {
+            CategoryViewModel cat = db.Categories.Where(c => c.CategoryId == id).Select(c => new CategoryViewModel()
+            {
+                CategoryId = c.CategoryId,
+                Name = c.Name,
+                Description = c.Description
+            }).FirstOrDefault();
+
+            if (cat == null)
+                return NotFound();
+            return Ok(cat);
+        }
+
+        //api/Categories (HttpPost)
+        public IHttpActionResult PostCategory(CategoryViewModel cat)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid Data");
+            }
+
+            db.Categories.Add(new Category()
+            {
+                Name = cat.Name,
+                Description = cat.Description
+            });
+
+            db.SaveChanges();
+            return Ok();
+        }
+
+        //api/Categories (HttpPut)
+        public IHttpActionResult PutCategory(CategoryViewModel cat)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid Data");
+            Category existingCategory = db.Categories.Where(c => c.CategoryId == cat.CategoryId).FirstOrDefault();
+
+            if (existingCategory != null)
+            {
+                existingCategory.Name = cat.Name;
+                existingCategory.Description = cat.Description;
+                db.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        //api/Categories/id (HttpDelete)
+        public IHttpActionResult DeleteCategory(int id)
+        {
+            Category cat = db.Categories.Where(c => c.CategoryId == id).FirstOrDefault();
+
+            if (cat != null)
+            {
+                db.Categories.Remove(cat);
+                db.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+    }
+}
